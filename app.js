f8485dcaa539ce786a90d433f26c5b4ca1bda8eb
Vlad Blank <blankvladp@gmail.com>
2020-05-04 18:42:54 +0300

    add check for contrast logic

--- app.js
+++ app.js
@@ -16,12 +16,32 @@ const generateHex = () => {
 // Display colors function
 const displayColor = () => {
   colorContainers.forEach( (container) => {
+    // get the h2 from all color containers
     const hexText = container.children[0];
-    const hexCode = generateHex();
+    // generate a random hex color code
+    const hexColor = generateHex();
 
-    container.style.backgroundColor = hexCode;
-    hexText.innerText = hexCode;
+    // update each color containers background color
+    container.style.backgroundColor = hexColor;
+    // update each color containers h2 text
+    hexText.innerText = hexColor;
+
+
+    // check the contrast for each color container and modify the color of the h2 based on the color's contrast
+    checkContrast(hexColor, hexText);
   });
+}
+
+// Check contrast function
+const checkContrast = (color, text) => {
+  // luminance() will generate a number between 0 and 1 -> 0: darker 1: lighter
+  const luminance = chroma(color).luminance();
+
+  if(luminance > 0.5) {
+    text.style.color = 'black';
+  } else {
+    text.style.color = 'white';
+  }
 }
 
 displayColor();
